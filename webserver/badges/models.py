a566a8b49e7779dceaa359545f212a7f22192b59
Jon Simington <jswv9@mst.edu>
2014-08-27 15:39:55 -0500

    Specify path to badge image on profile page

--- webserver/badges/models.py
+++ webserver/badges/models.py
@@ -19,6 +19,7 @@ class CompetitionVeteranBadge(Badge):
         "Silver",
         "Gold",
     ]
+    description = "For those who have competed in MegaMinerAI Competitions"
     events = [
         "competition_finished",
     ]
@@ -28,11 +29,15 @@ class CompetitionVeteranBadge(Badge):
         try:
           repo_head = team.teamclient.repository.repo.head()
           base_head = team.teamclient.base.repository.repo.head()
+
           return repo_head != base_head
         except ObjectDoesNotExist:
+          logger.debug("DoesNotExist")
           return False
         except KeyError:
+          logger.debug("KeyError")
           return False
+
 
     def award(self, user=None, **state):
         logger.debug("Checking {} for Veteran".format(user.username))
@@ -40,18 +45,22 @@ class CompetitionVeteranBadge(Badge):
             logger.warning("Must provide a user for Veteran badge")
 
         participated = 0
+
+        # users who have participated are not getting badges...
         for team in user.team_set.all():
             if self.has_participated(team):
                 participated += 1
 
         if participated > 5:
             logger.debug("Awarding Veteran Level 3 to {}".format(user.username))
-            return BadgeAwarded(level=3)
+            return BadgeAwarded(level=2)
         elif participated > 3:
             logger.debug("Awarding Veteran Level 2 to {}".format(user.username))
-            return BadgeAwarded(level=2)
+            return BadgeAwarded(level=1)
         elif participated > 0:
             logger.debug("Awarding Veteran Level 1 to {}".format(user.username))
-            return BadgeAwarded(level=1)
+            return BadgeAwarded(level=0)
+        else:
+            return
 
 badges.register(CompetitionVeteranBadge)
